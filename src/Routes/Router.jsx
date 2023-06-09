882bc3e16a569d7054f4a77eef03d0e2b729e1ad
Mustakim <sycospike1@gmail.com>
2023-06-09 17:18:18 +0600

    added 404 page

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -5,11 +5,13 @@ import {
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Instructors from "../Pages/Instructors/Instructors";
+import Error from "../Pages/Error/Error";
 
  export const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <Error></Error>,
       children: [
         {
             path: '/',
